3e5524b758487a8ab5859eb3b02ade90167b3153
Dannus90 <persson.daniel.1990@gmail.com>
2020-09-20 11:59:58 +0200

    Add Data annotations for Reservation Model

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -7,10 +7,23 @@ namespace ExpressoAPI.Models
 
         [Required]
         public string Name { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
         public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("^[0-9]*$")]
         public string Phone { get; set; }
+
+        [Required]
         public int TotalPeople { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
         public string Time { get; set; }
 
         public Reservation()
